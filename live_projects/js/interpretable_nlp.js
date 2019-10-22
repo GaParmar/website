542f5397232456ec4af2a0ff8644778c19d3e999
GaParmar <gparmar@ucsd.edu>
2019-10-21 22:20:35 -0700

    added initial frontend nlp project

---
+++ live_projects/js/interpretable_nlp.js
@@ -0,0 +1,93 @@
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+};
+
+function generateOnClick(){
+    var data = {
+        "sentence": document.getElementById('sentence').value
+    }
+}
+
+var sent = ["this", "apple", "is", "delicious"]
+
+var colr = Chart.helpers.color;
+
+var word_weight_data = {
+    labels: sent,
+    datasets: [{
+        label: 'Model 1: bigram model',
+        backgroundColor: colr(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.red,
+        borderWidth: 1,
+        data: [1,2,3,4]
+    }, {
+        label: 'Model 2: word2vec model',
+        backgroundColor: colr(window.chartColors.blue).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.blue,
+        borderWidth: 1,
+        data: [4,3,2,1]
+    }]
+};
+
+var phrase_weight_data = {
+    labels: sent,
+    datasets: [{
+        label: 'Model 1: bigram model',
+        backgroundColor: colr(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.red,
+        borderWidth: 1,
+        data: [1,2,3,4]
+    }]
+};
+
+window.onload = function(){
+    let ctx = document.getElementById('word_weight').getContext('2d');
+    window.wordWeightBar = new Chart(ctx, {
+        type: 'bar',
+        data: word_weight_data,
+        options: {
+            responsive: false,
+            title: {
+                display: true,
+                text: 'Word weight'
+            },
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                    beginAtZero: true
+                    }
+                }]
+            }
+        },
+
+    });
+
+    var bi_canvas = document.getElementById('bigram_weight').getContext('2d');
+    window.bigramBar = new Chart(bi_canvas, {
+        type: 'bar',
+        data: phrase_weight_data,
+        options: {
+            responsive: false,
+            title: {
+                display: true,
+                text: 'Bigram Weight'
+            },
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                    beginAtZero: true
+                    }
+                }]
+            }
+        },
+
+    });
+}
---
+++ live_projects/js/particle_background.js
@@ -0,0 +1,106 @@
+var allParticles = [];
+var maxLevel = 3;
+var useFull = false;
+var data = [];
+var friction = 0.9
+
+// Moves to a random direction and comes to a stop. 
+// Spawns other particles within its lifetime.
+function Particle(x, y, level){
+    this.level = level;
+    this.life = 0;
+
+    this.pos = new p5.Vector(x,y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(map(this.level, 0, maxLevel, 5, 2));
+    
+    this.move = function(){
+        this.life++;
+
+        // friction
+        this.vel.mult(friction);
+        this.pos.add(this.vel);
+
+        // Create a new particle if conditions are met
+        if (this.life%10 == 0){
+            if(this.level>0){
+                this.level -= 1;
+                var newParticle = new Particle(this.pos.x, this.pos.y, this.level-1);
+                allParticles.push(newParticle);
+            }
+        }
+    }
+}
+
+opacity=30
+
+function setup(){
+    createCanvas(windowWidth, windowHeight*1.5);
+    colorMode(HSB, 360);
+    textAlign(CENTER);
+    background('rgba(255,255,255, 0.25)');
+}
+
+function draw(){
+    // Create fade effect
+    noStroke();
+    fill('rgba(255,255,255, 0.25)');
+    // fill('#3B474B', 30);
+    rect(0,0,width, height);
+
+    // Move and spawn particles.
+    // Remove any that is below the velocity threshold
+    for(var i = allParticles.length-1; i>-1; i--){
+        allParticles[i].move();
+        if(allParticles[i].vel.mag() < 0.01){
+            allParticles.splice(i,1);
+        }
+    }
+
+    if (allParticles.length > 0){
+        // run script to get points to create triangles with
+        data = Delaunay.triangulate(allParticles.map(function(pt){
+            return [pt.pos.x, pt.pos.y];
+        }));
+
+        strokeWeight(0.1);
+
+        // Display triangles individually.
+        for (var i = 0; i<data.length; i+=3){
+            var p1 = allParticles[data[i]];
+            var p2 = allParticles[data[i+1]];
+            var p3 = allParticles[data[i+2]];
+            // dont draw if triangle is too big
+            if (dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y)>75){
+                continue;
+            }
+            if (dist(p2.pos.x, p2.pos.y, p3.pos.x, p3.pos.y)>75){
+                continue;
+            }
+            if (dist(p1.pos.x, p1.pos.y, p3.pos.x, p3.pos.y)>75){
+                continue;
+            }
+            // Base its hue by the particle's life
+            noStroke();
+            fill(165+p1.life*1.5, 360, 360, opacity);
+            triangle(p1.pos.x, p1.pos.y,
+                     p2.pos.x, p2.pos.y,
+                     p3.pos.x, p3.pos.y);
+        }
+    }
+    noStroke();
+    fill('rgba(23,28,29, 0.25)');
+}
+
+
+// Will execute myCallback every 0.5 seconds 
+var intervalID = window.setInterval((
+    ()=>{
+        allParticles.push(new Particle(random(windowWidth), random(windowHeight), maxLevel))
+    }
+), 100);
+
+
+// function mouseMoved(){
+//     allParticles.push(new Particle(random(windowWidth), random(windowHeight), maxLevel))
+// }
